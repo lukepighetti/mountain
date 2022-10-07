becdcb26f57fdd0de78fbca663d619222771afe8
Luke Pighetti <lukepighetti@gmail.com>
2022-10-07 18:11:22 -0400

    add megaphone

---
+++ README.md
@@ -0,0 +1,7 @@
+# mountain
+
+I am stuck on this mountain and I'm not sure how to get off. 
+Ideas for what you can ask me:
+
+1) show map
+2) 
---
+++ megaphone/index.js
@@ -0,0 +1,28 @@
+const Client = require('tmi.js').Client;
+
+const channel = 'lukepighetti';
+const message = "Help me get off this mountain... it's too slippery!";
+
+const client = Client({
+  "channels": [channel],
+  identity: {
+    username: 'lukepighetti',
+    password: process.env.TWITCH_TOKEN,
+  },
+});
+
+const sendMessage = () => client.say(channel, message);
+
+async function main() {
+  await client.connect();
+
+  setTimeout(() => {
+    sendMessage();
+  }, 5 * 1000);
+
+  setInterval(() => {
+    sendMessage();
+  }, 60 * 1000);
+}
+
+main();
